ce0978b6adfc220b679233ae3eef7ce50a825644
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-05-09 15:44:50 -0700

    readme and GitHub Page setup

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ApplePodcast from "../assets/desktop/apple-podcast.svg";
 import GooglePodcast from "../assets/desktop/google-podcasts.svg";
 import SpotifyPodcast from "../assets/desktop/spotify.svg";
@@ -9,6 +9,27 @@ import "./Modal.css";
 const icons = [SpotifyPodcast, ApplePodcast, GooglePodcast, PocketCasts];
 
 const Modal = () => {
+  const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+  const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState({
+    email: "",
+  });
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    const validInputEmail = emailRegex.test(e.target.value);
+    if (!validInputEmail) {
+      setErrorMessage((prevErrors) => ({
+        ...prevErrors,
+        email: "Oops! Check your email",
+      }));
+    } else {
+      setErrorMessage((prevErrors) => ({
+        ...prevErrors,
+        email: "",
+      }));
+    }
+  };
   return (
     <div className="modal">
       <h1 className="modal__title">
@@ -25,10 +46,12 @@ const Modal = () => {
           type="email"
           placeholder="Email address"
           className="modal__input"
+          onChange={handleEmailChange}
         />
         <button type="submit" className="modal__button">
           Request Access
         </button>
+        <p className="modal__error">{errorMessage.email}</p>
       </div>
       <div className="modal__icons">
         {icons.map((icon, index) => (
